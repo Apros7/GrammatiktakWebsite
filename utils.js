5c9c5704a269a6b01defc6856cd6a2c664c2a238
apros7 <lucas.vilsen@gmail.com>
2023-08-09 12:26:13 +0200

    more oop

--- utils.js
+++ utils.js
@@ -1,16 +1,20 @@
 
 class VisualError {
-    constructor(error) {
+    constructor(error, sentence) {
         this.wrong_word = error[0]
         this.right_word = error[1]
         this.indexes = error[2]
         this.description = error[3]
+        this.sentence = sentence
         this.visual_representation = document.createElement("div")
         this.visual_representation.classList.add("error-message")
     }
+
+    update_sentence(sentence) {
+        this.sentence = sentence
+    }
     init_visual_representation() {
         this.visual_representation.append(this.close_button())
-
     }
 
     close_button() {
