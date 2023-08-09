a701cd142aa42d8b1f0617d43375fbd43fdefa09
apros7 <lucas.vilsen@gmail.com>
2023-08-09 08:26:44 +0200

    began refactoring handle_errors.js

---
+++ utils.js
@@ -0,0 +1,22 @@
+
+class VisualError {
+    constructor(error) {
+        this.wrong_word = error[0]
+        this.right_word = error[1]
+        this.indexes = error[2]
+        this.description = error[3]
+        this.visual_representation = document.createElement("div")
+        this.visual_representation.classList.add("error-message")
+    }
+    init_visual_representation() {
+        this.visual_representation.append(this.close_button())
+
+    }
+
+    close_button() {
+        const closeButton = document.createElement("div");
+        closeButton.classList.add("close-button");
+        closeButton.textContent = "X";
+    }
+
+}
