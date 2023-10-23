9c5cd62e94c3d0c56d5e93ae3a6e3a617775ae6a
apros7 <lucas.vilsen@gmail.com>
2023-10-23 11:34:48 +0200

    fetching bug

--- utils/fetching.js
+++ utils/fetching.js
@@ -1,8 +1,9 @@
 import { get_text } from "./retrieve_text.js";
 
-export function fetchFeedback(service_url, feedback = null) {
+export function fetchFeedback(service_url, feedback = null, text_failed = null) {
     if (feedback === null) {feedback = document.querySelector(".feedback-text").innerText;}
-    let object = {"sentence": get_text(), "feedback": feedback};
+    if (text_failed === null) {text_failed = get_text()}
+    let object = {"sentence": text_failed, "feedback": feedback};
     fetch(service_url, {
       method: 'POST',
       headers: {
@@ -13,30 +14,36 @@ export function fetchFeedback(service_url, feedback = null) {
 }
 
 export async function fetchData(service_url, text, sentence_information) {
-  console.log("Correcting: ", text)
-  let object = {"sentence": text, "feedback": null};
   try {
-    const response = await fetch(service_url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(object)
-    })
-    if (!response.ok) {
-      return "error"
-    }
-    const data = await response.text();
-    const errors = JSON.parse(data.replace(/\\u([a-f0-9]{4})/gi, (match, group) => String.fromCharCode(parseInt(group, 16))));
-    sentence_information.waiting_for_backend[text] = false
-    sentence_information.errors_matching_text[text] = errors
-    return errors
+    result = await _fetchData(service_url, text, sentence_information)
+    return result
   } catch (error) {
     console.log(`Error with: "${text}"`);
     sentence_information.waiting_for_backend[text] = false;
     sentence_information.errors_matching_text[text] = [];
+    fetchFeedback(service_url, "Automatic Feedback: Text Failed", text)
     return [];
   }
+}
+
+async function _fetchData(service_url, text, sentence_information) {
+  console.log("Correcting: ", text)
+  let object = {"sentence": text, "feedback": null};
+  const response = await fetch(service_url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(object)
+  })
+  if (!response.ok) {
+    return "error"
+  }
+  const data = await response.text();
+  const errors = JSON.parse(data.replace(/\\u([a-f0-9]{4})/gi, (match, group) => String.fromCharCode(parseInt(group, 16))));
+  sentence_information.waiting_for_backend[text] = false
+  sentence_information.errors_matching_text[text] = errors
+  return errors
 }
 
 function create_fetching_error_message() {
@@ -48,11 +55,10 @@ function create_fetching_error_message() {
     rightColumn.appendChild(errorText)
 }
 
-export function handle_fetching_error(status, service_url) {
-  const correctTextButton = document.querySelector(".submit-button")
-  // correctTextButton.innerText = "Ret min tekst";
-  if (status !== "error") {return status}
-  create_fetching_error_message()
-  // sent auto feedback in case of error
-  fetchFeedback(service_url, feedback="Automatic Feedback: Text Failed")
-}
+// export function handle_fetching_error(status, service_url) {
+//   const correctTextButton = document.querySelector(".submit-button")
+//   // correctTextButton.innerText = "Ret min tekst";
+//   if (status !== "error") {return status}
+//   create_fetching_error_message()
+//   // sent auto feedback in case of error
+// }
