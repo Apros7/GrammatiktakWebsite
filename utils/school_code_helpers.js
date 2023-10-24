03b1535c72e5023f37a4e809611de7962e4886e7
apros7 <lucas.vilsen@gmail.com>
2023-10-24 20:07:14 +0200

    cleaner code

--- utils/school_code_helpers.js
+++ utils/school_code_helpers.js
@@ -2,8 +2,10 @@
 
 function isNumericAndShort(str) { return /^\d+$/.test(str) && str.length < 10; }
 
+const tester_code = "7357"
+
 function code_valid(code) {
-    const codes = ["14187", "240506"] // should be secret, if you found this, hi ;)
+    const codes = ["14187", "240506", tester_code] // should be secret, if you found this, hi ;)
     if (!isNumericAndShort(code)) { return false }
     if (codes.some(c => c === code)) { return true }
     return false
